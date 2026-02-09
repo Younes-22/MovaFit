3526e7855d2bd1cc32489502a95a0e61c59817d2
Younes Bekhti <younesbekhti04.com>
2026-02-09 16:26:10 +0000

    connected to firebase db

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'services/auth_service.dart';
 
-void main() {
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MotivafitApp());
 }
+
 
 class MotivafitApp extends StatelessWidget {
   const MotivafitApp({super.key});
@@ -17,7 +29,37 @@ class MotivafitApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const LoginScreen(),
+      home: AuthTestPage(),
+    );
+  }
+}
+
+class AuthTestPage extends StatelessWidget {
+  AuthTestPage({super.key});
+
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Firebase Auth Test')),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Create test user'),
+          onPressed: () async {
+            try {
+              final result = await _auth.signUp(
+                email: 'test${DateTime.now().millisecondsSinceEpoch}@test.com',
+                password: 'password123',
+              );
+
+              debugPrint('User created: ${result.user?.uid}');
+            } catch (e) {
+              debugPrint('Auth error: $e');
+            }
+          },
+        ),
+      ),
     );
   }
 }
